6d3479a2be8fe2561ebb2b9725e94cd6ec97489b
adil tahiri <rajaadil1995@gmail.com>
2022-12-20 23:51:51 +0100

    add firebase connect and other env variable

---
+++ src/firebase.config.js
@@ -0,0 +1,20 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, firestore, storage };
