d0f0fbdde4ab0245970589d2542f66438b398dc6
adil tahiri <rajaadil1995@gmail.com>
2022-12-21 23:33:01 +0100

    add hero and categories data

---
+++ src/utils/data.jsx
@@ -0,0 +1,77 @@
+import Rice from "../assets/img/r5.png";
+import IceCream from "../assets/img/i7.png";
+import SeoFood from "../assets/img/fi1.png";
+import TandooriChicken from "../assets/img/c3.png";
+
+export const heroData = [
+  {
+    id: 1,
+    name: "iceCream",
+    decp: "strawberry vanilla",
+    price: 5.21,
+    src: IceCream,
+  },
+  { id: 2, name: "rice", decp: "vegetarian rice", price: 7, src: Rice },
+  {
+    id: 3,
+    name: "seo food",
+    decp: "fresh sea food",
+    price: 12.99,
+    src: SeoFood,
+  },
+  {
+    id: 4,
+    name: "Tandoori chicken",
+    decp: "fred special chicken",
+    price: 11.59,
+    src: TandooriChicken,
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Chicken",
+    URLSearchParams: "chicken",
+  },
+  {
+    id: 2,
+    name: "Beef",
+    URLSearchParams: "beef",
+  },
+  {
+    id: 3,
+    name: "Icecreams",
+    URLSearchParams: "icecreams",
+  },
+  {
+    id: 4,
+    name: "Fish",
+    URLSearchParams: "fish",
+  },
+  {
+    id: 5,
+    name: "Pizza",
+    URLSearchParams: "pizza",
+  },
+  {
+    id: 6,
+    name: "Fruit",
+    URLSearchParams: "fruit",
+  },
+  {
+    id: 7,
+    name: "Curry",
+    URLSearchParams: "curry",
+  },
+  {
+    id: 8,
+    name: "Rice",
+    URLSearchParams: "rice",
+  },
+  {
+    id: 8,
+    name: "soft Drinks",
+    URLSearchParams: "drinks",
+  },
+];
