fa8052d42674a93a1676061c244f34c0b83226f8
adil tahiri <rajaadil1995@gmail.com>
2022-12-28 23:56:34 +0100

    update export index add more component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,9 @@
 export { default as Header } from "./Header";
 export { default as CreateContainer } from "./CreateContainer";
-export { default as MainContainer } from "./MainContainer";
+export { default as Footer } from "./Footer";
+export { default as Hero } from "./Hero";
+export { default as CardItems } from "./CardItems";
+export { default as CardShopping } from "./CardShopping";
+export { default as MenuSection } from "./MenuSection";
+export { default as FoodSection } from "./FoodSection";
+export { default as Services } from "./Services";
