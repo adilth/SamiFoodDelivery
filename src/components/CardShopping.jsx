d7872655be43ac3049ba53b65fd46c0890183307
adil tahiri <rajaadil1995@gmail.com>
2023-01-01 23:36:42 +0100

    add total product

--- src/components/CardShopping.jsx
+++ src/components/CardShopping.jsx
@@ -7,7 +7,7 @@ import CardItems from "./CardItems";
 import { actionTypes } from "../context/reducer";
 import EmptyCart from "../assets/img/emptyCart.svg";
 import { showMenuCart } from "../utils/getAllData";
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+// import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
 function CardShopping() {
   const [{ foodCart, user }, dispatch] = useStateValue();
@@ -16,11 +16,11 @@ function CardShopping() {
   const [isPaymentLoading, setPaymentLoading] = useState(false);
   const showCart = showMenuCart();
   useEffect(() => {
-    let totalPrice = foodCart.reduce(function (accumulator, item) {
-      return accumulator + item.qty * item.price;
+    let totalPrice = foodCart?.reduce((accumulator, item) => {
+      return accumulator + item?.qty * item?.price;
     }, 0);
     setTot(totalPrice);
-    console.log(tot);
+    // console.log(foodCart);
   }, [tot, flag]);
 
   const clearCart = () => {
@@ -32,32 +32,32 @@ function CardShopping() {
     localStorage.setItem("food", JSON.stringify([]));
   };
 
-  const stripe = useStripe();
-  const elements = useElements();
-  const payMoney = async (e) => {
-    e.preventDefault();
-    if (!stripe || !elements) {
-      return;
-    }
-    setPaymentLoading(true);
-    const clientSecret = getClientSecret();
-    const paymentResult = await stripe.confirmCardPayment(clientSecret, {
-      payment_method: {
-        card: elements.getElement(CardElement),
-        billing_details: {
-          name: "adil Yusuff",
-        },
-      },
-    });
-    setPaymentLoading(false);
-    if (paymentResult.error) {
-      alert(paymentResult.error.message);
-    } else {
-      if (paymentResult.paymentIntent.status === "succeeded") {
-        alert("Success!");
-      }
-    }
-  };
+  // const stripe = useStripe();
+  // const elements = useElements();
+  // const payMoney = async (e) => {
+  //   e.preventDefault();
+  //   if (!stripe || !elements) {
+  //     return;
+  //   }
+  //   setPaymentLoading(true);
+  //   const clientSecret = getClientSecret();
+  //   const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+  //     payment_method: {
+  //       card: elements.getElement(CardElement),
+  //       billing_details: {
+  //         name: "adil Yusuff",
+  //       },
+  //     },
+  //   });
+  //   setPaymentLoading(false);
+  //   if (paymentResult.error) {
+  //     alert(paymentResult.error.message);
+  //   } else {
+  //     if (paymentResult.paymentIntent.status === "succeeded") {
+  //       alert("Success!");
+  //     }
+  //   }
+  // };
   return (
     <motion.div
       initial={{ opacity: 0, x: 200 }}
@@ -65,83 +65,81 @@ function CardShopping() {
       exit={{ opacity: 0, x: 200 }}
       className="fixed top-0 right-0 w-full md:w-275 h-screen bg-white drop-shadow-md flex flex-col z-[333]"
     >
-      <form action="" onSubmit={payMoney}>
-        <div
-          className="w-full flex items-center justify-between p-4 cursor-pointer"
-          onClick={showCart}
+      <div
+        className="w-full flex items-center justify-between p-4 cursor-pointer"
+        onClick={showCart}
+      >
+        <motion.div whileTap={{ scale: 0.75 }}>
+          <MdOutlineKeyboardBackspace className="text-textColor text-3xl" />
+        </motion.div>
+        <div className="text-textColor text-lg font-semibold">Cart</div>
+        <motion.p
+          whileTap={{ scale: 0.82 }}
+          onClick={clearCart}
+          className="flex items-center gap-2 p-1 bg-gray-100 rounded-md hover:shadow-md cursor-pointer text-textColor text-base"
         >
-          <motion.div whileTap={{ scale: 0.75 }}>
-            <MdOutlineKeyboardBackspace className="text-textColor text-3xl" />
-          </motion.div>
-          <div className="text-textColor text-lg font-semibold">Cart</div>
-          <motion.p
-            whileTap={{ scale: 0.82 }}
-            onClick={clearCart}
-            className="flex items-center gap-2 p-1 bg-gray-100 rounded-md hover:shadow-md cursor-pointer text-textColor text-base"
-          >
-            Clear <RiRefreshFill />
-          </motion.p>
+          Clear <RiRefreshFill />
+        </motion.p>
+      </div>
+
+      {/* items Section */}
+      {foodCart && foodCart?.length > 0 ? (
+        <div className="w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col">
+          <div className="w-full h-340 md:h-42 px-3 md:px-5 py-4 md:py-8 flex flex-col gap-3 overflow-y-scroll scrollbar-none">
+            {/* cart Item */}
+            {foodCart &&
+              foodCart.length > 0 &&
+              foodCart?.map((item) => (
+                <CardItems
+                  key={item.id}
+                  item={item}
+                  setFlag={setFlag}
+                  flag={flag}
+                />
+              ))}
+          </div>
+          {/* cart total section */}
+          <div className="w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
+            <div className="w-full flex items-center justify-between">
+              <p className="text-gray-400 text-lg">Sub Total</p>
+              <p className="text-gray-400 text-lg">$ {tot}</p>
+            </div>
+            <div className="w-full flex items-center justify-between">
+              <p className="text-gray-400 text-lg">Delivery</p>
+              <p className="text-red-500 text-lg">$ 2.5</p>
+            </div>
+
+            <div className="w-full border-b border-gray-600 my-2"></div>
+
+            <div className="w-full flex items-center justify-between">
+              <p className="text-gray-200 text-xl font-semibold">Total:</p>
+              <input
+                value={tot + 2.5}
+                readOnly
+                name="total"
+                className="text-gray-200 text-xl font-semibold border-none bg-cartTotal ml-2 w-min"
+                disabled
+              />
+            </div>
+            {/* <CardElement /> */}
+            {user ? (
+              <ButtonCheckOut
+                disabled={isPaymentLoading}
+                text={isPaymentLoading ? "Loading..." : "Pay"}
+              />
+            ) : (
+              <ButtonCheckOut text="Login to Check Out" />
+            )}
+          </div>
         </div>
-
-        {/* items Section */}
-        {foodCart && foodCart.length > 0 ? (
-          <div className="w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col">
-            <div className="w-full h-340 md:h-42 px-3 md:px-5 py-4 md:py-8 flex flex-col gap-3 overflow-y-scroll scrollbar-none">
-              {/* cart Item */}
-              {foodCart &&
-                foodCart.length > 0 &&
-                foodCart.map((item) => (
-                  <CardItems
-                    key={item.id}
-                    item={item}
-                    setFlag={setFlag}
-                    flag={flag}
-                  />
-                ))}
-            </div>
-            {/* cart total section */}
-            <div className="w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
-              <div className="w-full flex items-center justify-between">
-                <p className="text-gray-400 text-lg">Sub Total</p>
-                <p className="text-gray-400 text-lg">$ {tot}</p>
-              </div>
-              <div className="w-full flex items-center justify-between">
-                <p className="text-gray-400 text-lg">Delivery</p>
-                <p className="text-red-500 text-lg">$ 2.5</p>
-              </div>
-
-              <div className="w-full border-b border-gray-600 my-2"></div>
-
-              <div className="w-full flex items-center justify-between">
-                <p className="text-gray-200 text-xl font-semibold">Total:</p>
-                <input
-                  value={tot + 2.5}
-                  readOnly
-                  name="total"
-                  className="text-gray-200 text-xl font-semibold border-none bg-cartTotal ml-2 w-min"
-                  disabled
-                />
-              </div>
-              <CardElement />
-              {user ? (
-                <ButtonCheckOut
-                  disabled={isPaymentLoading}
-                  text={isPaymentLoading ? "Loading..." : "Pay"}
-                />
-              ) : (
-                <ButtonCheckOut text="Login to Check Out" />
-              )}
-            </div>
-          </div>
-        ) : (
-          <div className="w-full h-full flex flex-col items-center justify-center gap-6">
-            <img src={EmptyCart} className="w-300" alt="" />
-            <p className="text-xl text-textColor font-semibold">
-              Add some items to your cart
-            </p>
-          </div>
-        )}
-      </form>
+      ) : (
+        <div className="w-full h-full flex flex-col items-center justify-center gap-6">
+          <img src={EmptyCart} className="w-300" alt="" />
+          <p className="text-xl text-textColor font-semibold">
+            Add some items to your cart
+          </p>
+        </div>
+      )}
     </motion.div>
   );
 }
