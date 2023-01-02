76384d41e4e507189bb29d9ce61148ab5a2e00df
adil tahiri <rajaadil1995@gmail.com>
2023-01-02 23:42:39 +0100

    add foodCart to initial state and fix fooditems remrmber data

--- src/context/initialState.js
+++ src/context/initialState.js
@@ -1,7 +1,12 @@
-import { fetchUser } from "../utils/fetchStorage";
+import { fetchItemsCart, fetchUser } from "../utils/fetchStorage";
+import { fetchAllFood } from "../utils/firebaseFunc";
 
 const userInfo = fetchUser();
+const cartInfo = fetchItemsCart();
+const data = await fetchAllFood();
 export const initialState = {
   user: userInfo,
-  foodItems: null,
+  foodItems: null || data,
+  cartShow: false,
+  foodCart: cartInfo,
 };
