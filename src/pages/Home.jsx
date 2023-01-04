77a6ac8e0f27b715481694d2e483054270d4ac79
adil tahiri <rajaadil1995@gmail.com>
2023-01-04 23:14:58 +0100

    add services component to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,12 @@
-import { useEffect } from "react";
-import {
-  CardShopping,
-  FoodSection,
-  Hero,
-  MenuSection,
-  Services,
-} from "../components";
-import { useStateValue } from "../context/stateProvider";
+import { FoodSection, Hero, MenuSection, Services } from "../components";
 
 function Home() {
-  const [{ cartShow }, dispatch] = useStateValue();
-  useEffect(() => {}, [cartShow]);
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center">
       <Hero />
       <FoodSection />
       <Services />
       <MenuSection />
-      {cartShow && <CardShopping />}
     </div>
   );
 }
