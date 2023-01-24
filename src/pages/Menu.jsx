eb9dd81ab941b60294a95eec289f932664fcabca
adil tahiri <rajaadil1995@gmail.com>
2023-01-24 18:30:37 +0100

    remove unnecessary calls

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,11 +1,8 @@
-import FoodRows from "../components/FoodRows";
 import { useStateValue } from "../context/stateProvider";
 import { Search } from "../components";
 
 function Menu() {
   const [{ foodItems }, dispatch] = useStateValue();
-
-  //  const [filterCategory, setFilterCategory] = useState("Chicken");
   return (
     <section id="foodMenu" className="w-full h-auto flex flex-col items-center">
       <div className="w-full flex flex-col items-center">
