63dc99c97e8d325c739fd73e4880aa3c4a1d590d
adil <rajaadil1995@gmail.com>
2023-09-29 23:46:12 +0100

    sperate some code form food Rows

--- client/src/components/FoodRows.jsx
+++ client/src/components/FoodRows.jsx
@@ -1,20 +1,24 @@
-import { motion } from "framer-motion";
 import { useEffect, useRef } from "react";
-import { FaShoppingCart } from "react-icons/fa";
 import { Splide, SplideTrack, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import { useStateValue } from "../context/stateProvider";
 import { actionTypes } from "../context/reducer";
-import { Link } from "react-router-dom";
 import NotFoundImg from "./NotFoundImg";
-import { buttonTap } from "../animations/motion";
 import { foodRowsSides } from "../animations/splides";
 import { activeProduct } from "../utils/firebaseFunc";
-import Rating from "./Rating";
+import FoodItem from "./FoodItem";
 
 function FoodRows({ flag, data, splide }) {
   const rowFood = useRef();
   const [{ foodCart, user }, dispatch] = useStateValue();
+  const updateCartAndLocalStorage = (newCart) => {
+    dispatch({
+      type: actionTypes.SET_FOOD_CART,
+      foodCart: newCart,
+    });
+    localStorage.setItem("food", JSON.stringify(newCart));
+  };
+
   useEffect(() => {
     const savedCart = JSON.parse(localStorage.getItem("food"));
     if (savedCart) {
@@ -24,28 +28,22 @@ function FoodRows({ flag, data, splide }) {
       });
     }
   }, [dispatch]);
+  console.log("here");
   const handleAddToCart = async (item) => {
     // Check if the item is already in the cart
     const exist = foodCart.find((el) => el.id === item.id);
-
+    let updatedCart;
     if (exist) {
       // If the item exists in the cart, update its quantity
       exist.qty += 1;
-      dispatch({
-        type: actionTypes.SET_FOOD_CART,
-        foodCart: [...foodCart],
-      });
-      localStorage.setItem("food", JSON.stringify(foodCart));
+      updatedCart = [...foodCart];
     } else {
       // If the item is not in the cart, add it with a quantity of 1
       item.qty = 1;
-      dispatch({
-        type: actionTypes.SET_FOOD_CART,
-        foodCart: [...foodCart, item],
-      });
-      //here needs to add new items to localStorage
-      localStorage.setItem("food", JSON.stringify([...foodCart, item]));
+      updatedCart = [...foodCart, item];
     }
+    updateCartAndLocalStorage(updatedCart);
+
     if (user && !exist) {
       await activeProduct({
         id: Date.now(),
@@ -80,21 +78,8 @@ function FoodRows({ flag, data, splide }) {
         <SplideTrack>
           {data && data?.length > 0 ? (
             data.map((item) => (
-              <SplideSlide
-                key={item.id}
-                className=" py-2 bg-cardOverlay rounded-lg hover:drop-shadow-lg flex flex-col justify-evenly relative backdrop-blur-lg"
-              >
-                <div className="w-full flex items-center cursor-pointer justify-evenly">
-                  <ImgLink item={item} />
-                  <motion.div
-                    {...buttonTap}
-                    className="w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md -mt-8"
-                    onClick={() => handleAddToCart(item)}
-                  >
-                    <FaShoppingCart className="text-white" />
-                  </motion.div>
-                </div>
-                <OtherDetails item={item} />
+              <SplideSlide key={item.id}>
+                <FoodItem item={item} handleAddToCart={handleAddToCart} />
               </SplideSlide>
             ))
           ) : (
@@ -115,22 +100,11 @@ function FoodRows({ flag, data, splide }) {
       >
         {data && data.length > 0 ? (
           data.map((item) => (
-            <div
+            <FoodItem
               key={item.id}
-              className=" py-2 bg-cardOverlay rounded-lg hover:drop-shadow-lg flex flex-col justify-evenly relative backdrop-blur-lg"
-            >
-              <div className="w-full flex items-center justify-evenly">
-                <ImgLink item={item} />
-                <motion.div
-                  whileTap={{ scale: 0.75 }}
-                  className="w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md -mt-8"
-                  onClick={() => handleAddToCart(item)}
-                >
-                  <FaShoppingCart className="text-white" />
-                </motion.div>
-              </div>
-              <OtherDetails item={item} />
-            </div>
+              item={item}
+              handleAddToCart={handleAddToCart}
+            />
           ))
         ) : (
           <NotFoundImg />
@@ -140,36 +114,4 @@ function FoodRows({ flag, data, splide }) {
   }
 }
 
-function OtherDetails({ item }) {
-  return (
-    <div className="flex flex-col items-end justify-end mt-2 pr-4">
-      <p className="text-textColor font-semibold text-base md:text-lg">
-        {item?.title}
-      </p>
-      <p className="mt-1 text-sm text-gray-500"> {item?.calories}</p>
-      <div className="flex items-center gap-8">
-        <p className="text-lg text-headingColor font-semibold">
-          <span className="text-sm to-red-500">${item?.price}</span>
-        </p>
-      </div>
-      <Rating rating={4} size={"w-5 h5"} readOnly />
-    </div>
-  );
-}
-export function ImgLink({ item }) {
-  return (
-    <Link
-      to={`/food/${item?.id}`}
-      className="w-[68%] drop-shadow-2xl rounded-full"
-    >
-      <motion.div whileHover={{ scale: 1.1 }}>
-        <img
-          src={item?.imgURL}
-          alt={item?.title}
-          className="w-full h-full object-contain rounded-full"
-        />
-      </motion.div>
-    </Link>
-  );
-}
 export default FoodRows;
