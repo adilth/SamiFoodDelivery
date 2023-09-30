9fb03c685f319493febbd2db7968391ea2a77759
adil <rajaadil1995@gmail.com>
2023-09-30 23:57:13 +0100

    add dark mode

--- client/src/components/NotFoundImg.jsx
+++ client/src/components/NotFoundImg.jsx
@@ -4,7 +4,7 @@ function NotFoundImg() {
   return (
     <div className="w-full flex flex-col items-center justify-center">
       <img src={NotFound} className="h-340" />
-      <p className="text-xl text-headingColor font-semibold my-2">
+      <p className="text-xl text-headingColor dark:text-darkHeadingColor font-semibold my-2">
         Items Not Available
       </p>
     </div>
