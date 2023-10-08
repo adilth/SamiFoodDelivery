2d91b48569bbad1275268a706a2cd1203445f046
adil <rajaadil1995@gmail.com>
2023-10-08 23:30:03 +0100

    add dark mode

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -4,7 +4,7 @@ import S3 from "../assets/png/s3.png";
 function Services() {
   return (
     <section className="w-full my-4" id="services">
-      <h2 className="text-2xl font-semibold mb-6 capitalize relative text-headingColor before:absolute before:rounded-lg before:content before:w-16 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100 mr-auto">
+      <h2 className="text-2xl font-semibold mb-6 capitalize relative text-headingColor dark:text-darkHeadingColor before:absolute before:rounded-lg before:content before:w-16 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100 mr-auto">
         What We Serve
       </h2>
       <div className="w-full flex items-center justify-center gap-8 flex-col sm:flex-row">
@@ -40,7 +40,9 @@ function CardService({ img, title, text, heading }) {
         className="lg:w-[65%] md:w-[80%]  h-[200px] bg-cover"
       />
       <h4 className="py-4 font-bold">{heading}</h4>
-      <p className=" text-textColor text-[0.9rem]">{text}</p>
+      <p className=" text-textColor dark:text-darkTextColor text-[0.9rem]">
+        {text}
+      </p>
     </div>
   );
 }
