a1a3065590aea0139bd61c68885dc44b98c0d191
adil <rajaadil1995@gmail.com>
2023-10-11 00:13:25 +0100

    add rating info to show reviews

---
+++ client/src/components/foodDetails/RatingReviews.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Rating from "./Rating";
+
+function RatingReviews({ comments, size }) {
+  function ratingReviews() {
+    const rating = (
+      comments?.reduce((acc, comment) => acc + Number(comment.rating), 0) /
+      comments.length
+    ).toFixed(1);
+    return isNaN(rating) ? 0 : rating;
+  }
+  return (
+    <div className="flex items-center">
+      <p className="text-sm font-semibold">
+        {ratingReviews() === 0 ? "" : ratingReviews()}
+      </p>
+      <Rating rating={ratingReviews()} size={size} readOnly />
+      <p className=" text-xs">({comments.length} reviews)</p>
+    </div>
+  );
+}
+
+export default RatingReviews;
