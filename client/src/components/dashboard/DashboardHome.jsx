cb640a2979582ed18466bb08875b0ebdf586770e
adil <rajaadil1995@gmail.com>
2023-10-10 17:29:34 +0100

    move all file that relate to dashboard inside the folder

---
+++ client/src/components/dashboard/DashboardHome.jsx
@@ -0,0 +1,52 @@
+import { Suspense, lazy } from "react";
+import {
+  chartUsers,
+  chartConversion,
+  chartProduct,
+  chartRevenue,
+  barChartRevenue,
+  barChartVisit,
+} from "../../utils/data";
+import { LineCharts, ChartBar, ChartArea } from "../chartPage";
+import Loader from "../Loader";
+const TopBox = lazy(() => import("../chartPage/TopBox"));
+const ChartPie = lazy(() => import("../chartPage/ChartPie"));
+function DashboardHome() {
+  return (
+    <div className="grid xl:gap-5 lg:gap-4 gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 auto-rows-[minmax(160px,auto)] grid-flow-dense mt-7">
+      <div className="p-[14px] rounded-xl border-2 border-slate-400 dark:border-slate-800 col-span-1 row-span-3">
+        <Suspense fallback={<Loader />}>
+          <TopBox />
+        </Suspense>
+      </div>
+      <div className="p-[14px] rounded-xl border-2 border-slate-400 dark:border-slate-800 col-span-1">
+        <LineCharts {...chartUsers} />
+      </div>
+      <div className="p-[14px] rounded-xl border-2 border-slate-400 dark:border-slate-800 col-span-1">
+        <LineCharts {...chartRevenue} />
+      </div>
+      <div className="p-[14px] rounded-xl border-2 border-slate-400 dark:border-slate-800  col-span-1 row-span-3">
+        <Suspense fallback={<Loader />}>
+          <ChartPie />
+        </Suspense>
+      </div>
+      <div className="p-[14px] rounded-xl border-2 border-slate-400 dark:border-slate-800 col-span-1">
+        <LineCharts {...chartProduct} />
+      </div>
+      <div className="p-[14px] rounded-xl border-2 border-slate-400 dark:border-slate-800 col-span-1">
+        <LineCharts {...chartConversion} />
+      </div>
+      <div className="p-[14px] rounded-xl border-2 border-slate-400 dark:border-slate-800 col-span-1 md:col-span-2 row-span-2">
+        <ChartArea />
+      </div>
+      <div className="p-[14px] rounded-xl border-2 border-slate-400 dark:border-slate-800 col-span-1">
+        <ChartBar {...barChartRevenue} />
+      </div>
+      <div className="p-[14px] rounded-xl border-2 border-slate-400 dark:border-slate-800 col-span-1">
+        <ChartBar {...barChartVisit} />
+      </div>
+    </div>
+  );
+}
+
+export default DashboardHome;
