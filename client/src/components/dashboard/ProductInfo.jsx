a15d8a1e31caca8744f800f98a6a67d603d2d35c
adil <rajaadil1995@gmail.com>
2023-10-10 17:33:10 +0100

    mode single product info all activity about it

---
+++ client/src/components/dashboard/ProductInfo.jsx
@@ -0,0 +1,25 @@
+import { useMemo } from "react";
+import { productData } from "../../utils/singletData";
+import { useStateValue } from "../../context/stateProvider";
+import Single from "./Single";
+import { useParams } from "react-router-dom";
+
+function ProductInfo() {
+  const [{ foodItems, activity }] = useStateValue();
+  const { productId } = useParams();
+  // console.log(foodItems);
+  let filter = foodItems.filter((item) => item.id == productId);
+  console.log("product", filter[0]);
+  const data = useMemo(
+    () => productData(filter[0], activity),
+    [filter, activity]
+  );
+  console.log("data", data);
+  return (
+    <div>
+      <Single {...data} />
+    </div>
+  );
+}
+
+export default ProductInfo;
