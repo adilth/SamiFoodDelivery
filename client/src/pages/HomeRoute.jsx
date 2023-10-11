2a38d9d911eb513778a7e4ac80ece92960b5cea0
adil <rajaadil1995@gmail.com>
2023-10-11 22:48:16 +0100

    split some code with dynamic import

--- client/src/pages/HomeRoute.jsx
+++ client/src/pages/HomeRoute.jsx
@@ -1,8 +1,8 @@
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import { Suspense, lazy } from "react";
-import Home from "./Home";
+const Home = lazy(() => import("./Home"));
 const Menu = lazy(() => import("./Menu"));
-import FoodDetails from "./FoodDetails";
+const FoodDetails = lazy(() => import("./FoodDetails"));
 import {
   CardShopping,
   Footer,
@@ -11,25 +11,57 @@ import {
   PageNotFound,
 } from "../components";
 import { useStateValue } from "../context/stateProvider";
+const UsersOrder = lazy(() => import("./UserOrder"));
+const AboutUs = lazy(() => import("./AboutUs"));
 
 function HomeRoute() {
   const location = useLocation();
-  const [{ cartShow, foodItem }, dispatch] = useStateValue();
+  const [{ cartShow, user }] = useStateValue();
   return (
     <>
       <Header />
-      <main className="mt-12 md:mt-16 px-4 md:px-10 lg:px-14 py-6 w-full">
+      <main className="mt-12 md:mt-16 px-4 md:px-10 lg:px-14 py-6 w-full dark:bg-darkPrimary">
         <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Home />} />
           <Route
-            path="menu"
+            path="/"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/menu"
             element={
               <Suspense fallback={<Loader />}>
                 <Menu />
               </Suspense>
             }
           />
-          <Route path="/food/:nameId" element={<FoodDetails />} />
+          <Route
+            path="/food/:nameId"
+            element={
+              <Suspense fallback={<Loader />}>
+                <FoodDetails />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/aboutUs"
+            element={
+              <Suspense fallback={<Loader />}>
+                <AboutUs />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/userOrder"
+            element={
+              <Suspense fallback={<Loader />}>
+                {user ? <UsersOrder /> : <Navigate to="/" replace />}
+              </Suspense>
+            }
+          />
           <Route path="/404" element={<PageNotFound />} />
           <Route path="/*" element={<Navigate to="/404" />} />
         </Routes>
