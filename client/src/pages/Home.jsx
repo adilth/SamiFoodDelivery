071c6a901699a6c9dedb320a3b3c00a2757e2daf
adil <rajaadil1995@gmail.com>
2023-10-11 22:48:02 +0100

    split some code with dynamic import

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,15 @@
-import { FoodSection, Hero, MenuSection, Services } from "../components";
-
+import { Suspense, lazy } from "react";
+import { FoodSection, Hero, Loader, Services } from "../components";
+const MenuSection = lazy(() => import("../components/MenuSection"));
 function Home() {
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center">
       <Hero />
       <FoodSection />
       <Services />
-      <MenuSection />
+      <Suspense fallback={<Loader />}>
+        <MenuSection />
+      </Suspense>
     </div>
   );
 }
