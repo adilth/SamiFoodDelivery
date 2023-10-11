ccda97deb3f4aecd067036f5172ecdcf58962ec1
adil <rajaadil1995@gmail.com>
2023-10-11 22:50:01 +0100

    remove unneeded code

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -1,8 +1,6 @@
-import { useStateValue } from "../context/stateProvider";
 import { Search } from "../components";
 
 function Menu() {
-  const [{ foodItems }, dispatch] = useStateValue();
   return (
     <section id="foodMenu" className="w-full h-auto flex flex-col items-center">
       <div className="w-full flex flex-col items-center">
