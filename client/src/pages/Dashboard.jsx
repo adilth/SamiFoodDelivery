0bc79eebbd1b2fec249d6e1f9c75e254799427fc
adil <rajaadil1995@gmail.com>
2023-10-11 22:44:53 +0100

    add color

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import { DashboardLeftSection, DashboardRightSection } from "../components";
 
 function Dashboard() {
   return (
-    <div className="w-screen h-screen flex items-center bg-primary">
+    <div className=" h-screen flex items-center bg-primary">
       <DashboardLeftSection />
       <DashboardRightSection />
     </div>
