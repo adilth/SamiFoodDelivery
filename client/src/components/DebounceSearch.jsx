e5e74d6a3971514b7aeee872164aec617a706ac5
adil <rajaadil1995@gmail.com>
2023-10-12 19:38:57 +0100

    change props and fix problem with it

--- client/src/components/DebounceSearch.jsx
+++ client/src/components/DebounceSearch.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
-import { FaSearch } from "react-icons/fa";
+import { FaSearch } from "@react-icons/all-files/fa/FaSearch";
 
-function DebounceSearch({ value: initValue, onChange }) {
-  const [value, setValue] = useState(initValue);
+function DebounceSearch({ val, onChange }) {
+  const [value, setValue] = useState(val);
   useEffect(() => {
-    setValue(initValue);
-  }, [initValue]);
+    setValue(val);
+  }, [val]);
   useEffect(() => {
     const timeout = setTimeout(() => {
       onChange(value);
@@ -19,10 +19,10 @@ function DebounceSearch({ value: initValue, onChange }) {
         onChange={(e) => setValue(e.target.value)}
         type="text"
         placeholder="Search..."
-        className="p-3 rounded-xl border border-gray-400"
+        className="p-3 rounded-xl border border-gray-400 dark:bg-darkCardBody"
         value={value}
       />
-      <FaSearch className="absolute top-[50%] right-7 translate-x-full -translate-y-1/2 text-lg" />
+      <FaSearch className="absolute top-[50%] right-7 translate-x-full -translate-y-1/2 text-lg dark:text-darkHeadingColor" />
     </div>
   );
 }
