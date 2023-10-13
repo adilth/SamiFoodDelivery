0f00fae7ad42ff2e38384740821cf551234f8598
adil <rajaadil1995@gmail.com>
2023-10-13 22:34:49 +0100

    add data provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,14 +7,19 @@ import { reducer } from "./context/reducer";
 import StateProvider from "./context/stateProvider";
 import "./index.css";
 import { AlertStateProvider } from "./context/alertProvider";
+import DataProvider from "./context/DataProvider";
+import { DataInitialState } from "./context/dataInitialState";
+import { DataReducer } from "./context/dataReducer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <StateProvider initialState={initialState} reducer={reducer}>
-        <AlertStateProvider>
-          <App />
-        </AlertStateProvider>
+        <DataProvider initialState={DataInitialState} reducer={DataReducer}>
+          <AlertStateProvider>
+            <App />
+          </AlertStateProvider>
+        </DataProvider>
       </StateProvider>
     </Router>
   </React.StrictMode>
