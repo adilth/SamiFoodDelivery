04275180604112537377f050ce0a12902255017e
adil <rajaadil1995@gmail.com>
2023-10-20 22:41:49 +0100

    add terser and some change

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,21 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    minifyIdentifiers: false,
+    keepNames: true,
+  },
   build: {
     target: "esnext", // or "es2019",
+    minify: "terser",
     emptyOutDir: true,
     manifest: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
   },
 });
