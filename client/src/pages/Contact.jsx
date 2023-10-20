aff8e29f29029b5f65e31a428e43cad68d9b46f9
adil <rajaadil1995@gmail.com>
2023-10-20 15:43:41 +0100

    add paragragh above form

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -19,10 +19,13 @@ function Contact() {
     console.log(contactInfo);
   };
   return (
-    <>
-      <h2 className="text-2xl font-semibold capitalize relative text-headingColor dark:text-darkHeadingColor before:absolute before:rounded-lg before:content before:w-16 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100 mr-auto mb-6">
+    <div className="my-5">
+      <h2 className="text-2xl font-semibold capitalize relative text-textColor dark:text-darkHeadingColor before:absolute before:rounded-lg before:content before:w-16 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100 mr-auto mb-6">
         Contact Us
       </h2>
+      <p className="text-center text-headingColor dark:text-darkHeadingColor pt-3">
+        write us what the issue you face or want to add new thing or feather{" "}
+      </p>
       <div className="flex justify-center">
         <form
           className="w-[90%] md:w-[75%] border my-5 m-auto border-gray-300 rounded-lg p-6 flex flex-col items-center justify-center gap-4"
@@ -67,13 +70,13 @@ function Contact() {
 
           <button
             type="submit"
-            className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100 text-white"
+            className="bg-gradient-to-br from-orange-700 to-orange-600 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100 text-white"
           >
             Submit
           </button>
         </form>
       </div>
-    </>
+    </div>
   );
 }
 
