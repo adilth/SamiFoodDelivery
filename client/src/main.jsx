8e663ad71d32e7eda5274804838e9748812d6c2a
adil <rajaadil1995@gmail.com>
2023-10-21 23:18:48 +0100

    add provider for food provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,25 +3,28 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import { initialState } from "./context/initialState";
-import { reducer } from "./context/reducer";
+import { initialState, initialStateSet } from "./context/initialState";
+import { reducer, reducerSet } from "./context/reducer";
 import StateProvider from "./context/StateProvider";
 import "./index.css";
 import { AlertStateProvider } from "./context/alertProvider";
 import DataProvider from "./context/DataProvider";
 import { DataInitialState } from "./context/dataInitialState";
 import { DataReducer } from "./context/dataReducer";
+import FoodProvider from "./context/FoodProvider";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <Router>
-      <StateProvider initialState={initialState} reducer={reducer}>
-        <DataProvider initialState={DataInitialState} reducer={DataReducer}>
-          <AlertStateProvider>
-            <App />
-          </AlertStateProvider>
-        </DataProvider>
+      <StateProvider initialState={initialStateSet} reducer={reducerSet}>
+        <FoodProvider initialState={initialState} reducer={reducer}>
+          <DataProvider initialState={DataInitialState} reducer={DataReducer}>
+            <AlertStateProvider>
+              <App />
+            </AlertStateProvider>
+          </DataProvider>
+        </FoodProvider>
       </StateProvider>
     </Router>
   </React.StrictMode>
